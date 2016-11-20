912aa66fadafefba962afc57597bd4be57f713a4
Jakub Matjanowski <jakub@pockit.com>
2016-11-20 12:00:27 +0100

    successfully run electron

--- main.ts
+++ main.ts
@@ -2,4 +2,24 @@ import * as Electron from 'electron';
 
 class MainProcess{
 
+    /**
+     *
+     */
+    constructor(private _htmlFile:string) {
+    }
+
+    public createWindow(){
+        let win = new Electron.BrowserWindow({width: 800, height: 600});
+        win.on('closed', () => {
+            win = null
+        });
+        
+        // Or load a local HTML file
+        win.loadURL(`file://${__dirname}/${this._htmlFile}`);
+    }
 }
+
+Electron.app.on('ready',()=>{
+    let mainProcess = new MainProcess("index.html");
+    mainProcess.createWindow();
+});
